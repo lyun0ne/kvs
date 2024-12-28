d288d4ccea06f20032b723bbf8833cab7fbba9b7
lyun <yun803453@gmail.com>
2024-12-28 19:53:48 +0800

    finished project_1

--- src/kv.rs
+++ src/kv.rs
@@ -1,20 +1,30 @@
+use std::collections::HashMap;
+
 /// The `KvStore` stores string key/value pairs.
 ///
 /// Key/value pairs are stored in a `HashMap` in memory and not persisted to disk.
 ///
-pub struct KvStore {}
+pub struct KvStore {
+    element: HashMap<String, String>,
+}
 
 impl KvStore {
     /// creat a kv
     pub fn new() -> Self {
-        unimplemented!()
+        KvStore {
+            element: HashMap::new(),
+        }
     }
     /// get value
-    pub fn get(_key: String) {
-        unimplemented!()
+    pub fn get(&self, key: String) -> Option<String> {
+        self.element.get(&key).cloned()
     }
     /// set valuue
-    pub fn set(_key: String, _value: String) {
-        unimplemented!()
+    pub fn set(&mut self, key: String, value: String) {
+        self.element.insert(key, value);
+    }
+    /// remove a value
+    pub fn remove(&mut self, key: String) {
+        self.element.remove(&key);
     }
 }
