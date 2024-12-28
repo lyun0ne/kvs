b8f746aaa1bac19b661649dec218e483b879ef15
lyun <yun803453@gmail.com>
2024-12-28 10:54:40 +0800

    add dependency

---
+++ target/debug/deps/kvs-e9907b53737eb124.d
@@ -0,0 +1,7 @@
+E:\software\rust\tp-201\kvs\target\debug\deps\kvs-e9907b53737eb124.rmeta: src\lib.rs
+
+E:\software\rust\tp-201\kvs\target\debug\deps\libkvs-e9907b53737eb124.rlib: src\lib.rs
+
+E:\software\rust\tp-201\kvs\target\debug\deps\kvs-e9907b53737eb124.d: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/kvs.d
@@ -0,0 +1,5 @@
+E:\software\rust\tp-201\kvs\target\debug\deps\kvs.exe: src\bin\kvs.rs
+
+E:\software\rust\tp-201\kvs\target\debug\deps\kvs.d: src\bin\kvs.rs
+
+src\bin\kvs.rs:
